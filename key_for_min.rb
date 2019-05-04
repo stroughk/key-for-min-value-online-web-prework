de921a2831e8096e01eda76f407a660ecffb3cf0
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-04 22:16:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(sons_ages)
+  sons_ages={
+    0: "Evan",
+    8: "Anderson",
+    5: "Eric"
+  }
+  
+  do sons_ages.collect |age, name|
+  puts age.sort 
+end 
 
 end
